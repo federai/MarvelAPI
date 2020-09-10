6376975143b89574392b07e21e3588d331494d75
Federico Raimondo <federico.raimondo@claro.com.ar>
2020-09-10 17:48:12 -0300

    final

--- js/app.js
+++ js/app.js
@@ -1,7 +1,8 @@
-const url='https://gateway.marvel.com:443/v1/public/characters?&limit=100&ts=1&apikey=7d3efefb40aafffc7ddbec0348905a27&hash=56d298663a24b3e08f10bbb54d2b2b7e';
+const url='https://gateway.marvel.com:443/v1/public/characters?&limit=25&ts=1&apikey=7d3efefb40aafffc7ddbec0348905a27&hash=56d298663a24b3e08f10bbb54d2b2b7e';
+function draw(){
 var contenido = document.querySelector('#marvelhero')
 const search = document.querySelector('search')
-function draw(){
+
 fetch (url)
     .then (res => res.json() )
     .then (datos=> {
@@ -17,18 +18,16 @@ function heroes(datos) {
         //console.log(valor.name)
        contenido.innerHTML+=`
                 <div class="col-6 col-md-4 card">
-                <div><img src="${hero.thumbnail.path}.${hero.thumbnail.extension}" alt="${hero.name}" class="card-img-top"></div>
-                <div class="card-body"><h1><a href="${hero.urls[0].url}" target="_blank"> ${hero.name} </a> </h1></div>
+                <div class="d-flex justify-content-center"><img src="${hero.thumbnail.path}.${hero.thumbnail.extension}" alt="${hero.name}" class="card-img-top"></div>
+                <div class="card-body"><h1><a id="link" href="${hero.urls[0].url}" target="_blank" > ${hero.name} </a> </h1></div>
                
             </div>`;
     }
-}}
-
+}
+}
 draw();
 
-function getRandomInt(min, max) {
-    return Math.floor(Math.random() * (max - min + 1)) + min;
-}
+
 function getSuperhero(e) {
     let xmlhttp = new XMLHttpRequest();
     let superhero = document.getElementById('superhero').value;
@@ -45,20 +44,13 @@ function getSuperhero(e) {
             let output = '';
   
             let totalRecords = response.data.results.length;
-            let randomSuperhero = getRandomInt(0, totalRecords - 1);
-  
-            let backgroundSuperheroPath = response.data.results[randomSuperhero].thumbnail.path;
-            let backgroundSuperheroExtension = response.data.results[randomSuperhero].thumbnail.extension;
-            let backgroundSuperheroImage = backgroundSuperheroPath + '.' + backgroundSuperheroExtension;
-            // console.log(randomSuperhero);
-            // console.log(backgroundSuperheroPath + '.' + backgroundSuperheroExtension);
-           
+            
   
             for (let i = 0; i < response.data.results.length; i++) {
               
                 output += '<div class="col-6 col-md-4 card">';
-                output += '<div><img src=' + response.data.results[i].thumbnail.path + '.' + response.data.results[i].thumbnail.extension + ' alt="" class="card-img-top"></div>'
-                output += '<div class="card-body"><h1><a target="_blank" href="' + response.data.results[i].urls[0].url + '">' + response.data.results[i].name + ' </a></h1></div>';
+                output += '<div class="d-flex justify-content-center"><img src=' + response.data.results[i].thumbnail.path + '.' + response.data.results[i].thumbnail.extension + ' alt="" class="card-img-top"></div>'
+                output += '<div class="card-body"><h1><a id="link" target="_blank" href="' + response.data.results[i].urls[0].url + '">' + response.data.results[i].name + ' </a></h1></div>';
                 output += '<div><p>' + response.data.results[i].description + '</p></div>';
                 output += '</div>';
             }
@@ -70,9 +62,7 @@ function getSuperhero(e) {
     return false;
   }
 
-    
-
-    
 
 
-    
+
+
